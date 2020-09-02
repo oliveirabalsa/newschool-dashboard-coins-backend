f7e25be5c7b1c36f1fec9629aa4646634b9e9f93
lucas <lucascardosopsd@gmail.com>
2020-09-02 16:07:28 -0300

    fix: get money route

--- dist/user-module/user-module.service.d.ts
+++ dist/user-module/user-module.service.d.ts
@@ -1,6 +1,7 @@
 declare class UserModuleService {
     getUser(): Promise<any>;
     getTransactions(id: any, query: any): Promise<any>;
+    getMoney(id: any): Promise<any>;
     postUser(payload: any): Promise<any>;
     putUser(payload: any, id: any): Promise<any>;
     deleteUser(payload: any): Promise<any>;
