a4046568ae224729906f6dd2c27ab2e6cba9a734
lucas <lucascardosopsd@gmail.com>
2020-09-02 13:45:32 -0300

    feat: get user coins

--- dist/user-module/user-module.controller.d.ts
+++ dist/user-module/user-module.controller.d.ts
@@ -3,6 +3,7 @@ export declare class UserModuleController {
     get(): Promise<any>;
     getTransactions(Params: any, query: any): Promise<any>;
     create(User: UserDto): Promise<any>;
+    getMoney(params: any): Promise<any>;
     update(params: any, User: UserDto): Promise<any>;
     updateMoneyQuantity(params: any, Money: any): Promise<any>;
     delete(params: any): Promise<any>;
