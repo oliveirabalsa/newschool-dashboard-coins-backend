d1e4ff3bb5789b99b035012c238d676ccc8d604b
Leonardo <oliveirabalsa2@gmail.com>
2020-09-03 17:32:53 -0300

    refactor: controllers

--- migrations/00_users.js
+++ migrations/00_users.js
@@ -1,11 +1,12 @@
 exports.up = async function (knex) {
-    await knex.schema.createTable('user', (table) => {
-        table.increments('id').primary();
-        table.string('moneyQuantity').notNull();
-        table.string('name').notNull();
-        table.string('type').notNull();
-        table.string('transactions').notNull();
-    });
+  await knex.schema.createTable('user', (table) => {
+    table.increments('id').primary();
+    table.string('moneyQuantity').notNull();
+    table.string('name').notNull();
+    table.string('type').notNull();
+    table.string('email').notNull();
+    table.string('transactions').notNull();
+  });
 };
 exports.down = (knex) => knex.schema.dropTableIfExists('user');
-//# sourceMappingURL=00_users.js.map
+// # sourceMappingURL=00_users.js.map
