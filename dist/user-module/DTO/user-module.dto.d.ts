552efd2dd93d5c5147a9f8703a5e2e1a57f4edda
Leonardo <oliveirabalsa2@gmail.com>
2021-03-24 18:26:39 -0300

    fix: implement some

---
+++ dist/user-module/DTO/user-module.dto.d.ts
@@ -0,0 +1,12 @@
+export declare class TransactionDto {
+    date: string;
+    quantity: string;
+    admin: string;
+    user_id: string;
+}
+export declare class UserDto {
+    name: string;
+    email: string;
+    moneyQuantity: string;
+    type: string;
+}
